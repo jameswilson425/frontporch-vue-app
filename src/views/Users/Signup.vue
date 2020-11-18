c3ad4eb37160d052c0209abb8d609921a92e1523
jimmy <jimewilson87@gmail.com>
2020-11-18 14:46:49 -0600

    updates signup view

--- src/views/Users/Signup.vue
+++ src/views/Users/Signup.vue
@@ -1,59 +1,70 @@
 <template>
   <div class="signup">
-    <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors">{{ error }}</li>
-      </ul>
-      <div class="form-group">
-        <label>Username:</label>
-        <input type="text" class="form-control" v-model="userName" /> <br />
-        <small>{{ 20 - userName.length }} characters remaining</small>
-      </div>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" /> <br />
-        <small v-if="password.length < 8" class="text-danger"
-          >Must be at least 8 characters</small
-        >
-        <small v-if="password.length > 20" class="text-danger"
-          >Cannot exceed 20 characters</small
-        >
-      </div>
-      <div class="form-group">
-        <label>Password confirmation:</label>
-        <input
-          type="password"
-          class="form-control"
-          v-model="passwordConfirmation"
-        />
-        <br />
-        <small v-if="password !== passwordConfirmation" class="text-danger"
-          >passwords do not match</small
-        >
-      </div>
-      <div class="form-group">
-        <label>Address:</label>
-        <input type="text" class="form-control" v-model="address" />
-      </div>
-      <div class="form-group">
-        <label>Bio:</label> <br />
-        <!-- <input type="text" class="form-control" v-model="bio" /> <br /> -->
-        <textarea name="bio" rows="10" cols="100">
+    <section class="bg-dark">
+      <div class="col-lg-4 col-md-6 sm-margin-50px-bottom form-center">
+        <form class="text-light bg-dark" v-on:submit.prevent="submit()">
+          <h1 class="text-light bg-dark">Signup</h1>
+          <ul>
+            <li class="text-danger" v-for="error in errors">{{ error }}</li>
+          </ul>
+          <div class="form-group">
+            <label>Username:</label>
+            <input type="text" class="form-control" v-model="userName" />
+            <small>{{ 20 - userName.length }} characters remaining</small>
+          </div>
+          <div class="form-group">
+            <label>Email:</label>
+            <input type="email" class="form-control" v-model="email" />
+          </div>
+          <div class="form-group">
+            <label>Password:</label>
+            <input type="password" class="form-control" v-model="password" />
+            <small v-if="password.length < 8" class="text-danger"
+              >Must be at least 8 characters</small
+            >
+            <small v-if="password.length > 20" class="text-danger"
+              >Cannot exceed 20 characters</small
+            >
+          </div>
+          <div class="form-group">
+            <label>Password confirmation:</label>
+            <input
+              type="password"
+              class="form-control"
+              v-model="passwordConfirmation"
+            />
+
+            <small v-if="password !== passwordConfirmation" class="text-danger"
+              >passwords do not match</small
+            >
+          </div>
+          <div class="form-group">
+            <label>Address:</label>
+            <input type="text" class="form-control" v-model="address" />
+          </div>
+          <div class="form-group">
+            <label>Bio:</label> <br />
+            <!-- <input type="text" class="form-control" v-model="bio" /> <br /> -->
+            <textarea name="bio" class="form-control" rows="10" cols="100">
 Tell us about yourself! Feel free to include links to your music and whatnot.</textarea
-        >
-        <small>{{ 900 - bio.length }} characters remaining</small>
+            >
+            <small>{{ 900 - bio.length }} characters remaining</small>
+          </div>
+          Profile Picture:
+          <input type="text" class="form-control" v-model="newProfileImage" />
+          <br />
+          <input type="submit" class="btn btn-secondary" value="Submit" />
+        </form>
       </div>
-      Profile Picture:
-      <input type="text" v-model="newProfileImage" />
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
+    </section>
   </div>
 </template>
+
+<style scoped>
+.form-center {
+  margin: 0 auto;
+}
+</style>
 
 <script>
 import axios from "axios";
